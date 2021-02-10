0de33377db8babfe997b8d2b004d3a37d7110d03
Juan Antonio Sicardo Contreras <contacto@juansicardo.com>
2021-02-09 18:53:51 -0600

    Made some changes

---
+++ practiceexercises/matrixmultiplication.c
@@ -0,0 +1,9 @@
+/*Author: Juan Antonio Sicardo Contreras
+* Date: Fall of 2019
+* Description: This program makes the multiplication of two matrixes. First, the program reads
+* from the user the sizes of the two matrixes that are going to be multiplied. Then, the program
+* reads from the input each element of both matrixes and finally it outputs the result.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
